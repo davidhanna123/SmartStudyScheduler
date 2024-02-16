05412b19131abca2bb378ded9b19c3b35895a943
Yadon Kassahun <yekassah@gmail.com>
2024-02-16 00:23:01 -0500

    Event classes

---
+++ src/newBusinessLogic/Event.java
@@ -0,0 +1,65 @@
+package newBusinessLogic;
+//Abstract class that other different kinds of event will extend
+public abstract class Event implements Comparable<Event>{
+	protected String title;
+	protected String description;
+	protected Hour startingTime;
+	protected int duration;
+	
+	
+	protected Homework homework;
+	protected Assignment assignment; 
+
+	protected Event(String title, String description, Hour startingTime, int duration) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.title = title;
+		this.description = description;
+		this.startingTime = startingTime;
+		this.duration = duration;
+	}
+	
+	public void setHomework(Homework homework) {
+		this.homework = homework;
+		// logic to set up the duration time based on hw's completion time
+		// here ...............
+	}
+	
+	public void setAssignment(Assignment assignment) {
+		this.assignment = assignment;
+		// logic to set up the duration time based on assignment's completion time
+		// here ...............
+	}
+
+	protected abstract String getTitle();
+	
+	protected abstract String getDescription();
+
+	protected abstract Hour getStartingTime();
+
+	protected abstract int getDuration();
+
+	protected abstract void setTitle(String title);
+
+	protected abstract void setDescription(String description);
+
+	protected abstract void setStartingTime(Hour startingTime);
+
+	protected abstract void setDuration(int duration);
+
+	@Override
+	public int compareTo(Event o) {
+		// comparing startingTime 
+		int compared = this.startingTime.compareTo(o.startingTime);
+		if (compared != 0) {
+			return compared;
+		}
+		
+		// if startingTimes are the same, then we compare by title
+		// it would prevent them from both being added to the set 
+		return this.title.compareTo(o.title);
+	}
+	
+	
+
+}
---
+++ src/newBusinessLogic/RepeatingEvent.java
@@ -0,0 +1,21 @@
+package newBusinessLogic;
+//Repeating event(like reoccuring labs, homeworks, assignments etc)*
+public class RepeatingEvent extends Event{
+	private String repeatRate;
+	private int repeatGapDay;
+	private int repeatGapYear;
+	private Day endRepeatDay;
+	private Month endRepeatMonth;
+	private Year endRepeatYear;
+
+	public RepeatingEvent() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	public void AddRepeatingEvent(Event event, int repeat) {
+		for (int i =0; i < repeat; i++) {
+			
+		}
+	}
+}
