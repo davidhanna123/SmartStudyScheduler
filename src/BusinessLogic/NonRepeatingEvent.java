fd661e76e3ce0a25f10e886d7b731b3ca8fc0eb5
Yadon Kassahun <yekassah@gmail.com>
2024-02-21 18:04:20 -0500

    Adding Javadoc comments to the NonRepeatingEvent class and the Year class.

--- src/BusinessLogic/NonRepeatingEvent.java
+++ src/BusinessLogic/NonRepeatingEvent.java
@@ -1,76 +1,118 @@
 package BusinessLogic;
-//Normal event*
+/**
+ * A non-repeating event that is a subclass of the Event abstract class.
+ * This event can be added to a Day by using methods in the Day class.
+ */
 public class NonRepeatingEvent extends Event{
 
+	/**
+     * Constructs a new NonRepeatingEvent object.
+     */
 	public NonRepeatingEvent() {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+     * Retrieves the title of the non-repeating event.
+     * 
+     * @return The title of the event.
+     */
 	@Override
 	public String getTitle() {
 		// TODO Auto-generated method stub
 		return this.title;
 	}
-
+	 /**
+     * Retrieves the description of the non-repeating event.
+     * 
+     * @return The description of the event.
+     */
 	@Override
 	public String getDescription() {
 		// TODO Auto-generated method stub
 		return this.description;
 	}
-
+	/**
+     * Retrieves the starting time of the non-repeating event.
+     * 
+     * @return The starting time of the event.
+     */
 	@Override
 	public Hour getStartingTime() {
 		// TODO Auto-generated method stub
 		return this.startingTime;
 	}
-
+	/**
+     * Retrieves the duration of the non-repeating event.
+     * 
+     * @return The duration of the event.
+     */
 	@Override
 	public int getDuration() {
 		// TODO Auto-generated method stub
 		return this.duration;
 	}
-
+	/**
+     * Sets the title of the non-repeating event.
+     * 
+     * @param title The title to be set for the event.
+     */
 	@Override
 	public void setTitle(String title) {
 		// TODO Auto-generated method stub
 		this.title = title;
 		
 	}
-
+	/**
+     * Sets the description of the non-repeating event.
+     * 
+     * @param description The description to be set for the event.
+     */
 	@Override
 	public void setDescription(String description) {
 		// TODO Auto-generated method stub
 		this.description = description;
 		
 	}
-
+	/**
+     * Sets the starting time of the non-repeating event.
+     * 
+     * @param startingTime The starting time to be set for the event.
+     */
 	@Override
 	public void setStartingTime(Hour startingTime) {
 		// TODO Auto-generated method stub
 		this.startingTime = startingTime;
 	}
-
+	/**
+     * Sets the duration of the non-repeating event.
+     * 
+     * @param duration The duration to be set for the event.
+     */
 	@Override
 	public void setDuration(int duration) {
 		// TODO Auto-generated method stub
 		this.duration = duration;
 		
 	}
-
+	/**
+     * Sets the homework associated with the non-repeating event.
+     * 
+     * @param homework The homework to be associated with the event.
+     */
 	@Override
 	public void setHomework(Homework homework) {
 		// TODO Auto-generated method stub
 		super.setHomework(homework);
 	}
-
+	/**
+     * Sets the assignment associated with the non-repeating event.
+     * 
+     * @param assignment The assignment to be associated with the event.
+     */
 	@Override
 	public void setAssignment(Assignment assignment) {
 		// TODO Auto-generated method stub
 		super.setAssignment(assignment);
 	}
-	
-	
-	
-	
-
 }
--- src/BusinessLogic/Year.java
+++ src/BusinessLogic/Year.java
@@ -4,43 +4,81 @@
 package BusinessLogic;
 
 import java.util.*;
-
+/**
+ * Represents a year with its associated months.
+ * Implements the Comparable interface to enable comparison between years.
+ */
 public class Year implements Comparable<Year> {
     private int currentYear;
     private TreeSet<Month> months;
 
+    /**
+     * Constructs a Year object with the specified current year.
+     * Initializes the set of months.
+     * 
+     * @param currentYear The current year to set.
+     */
     public Year(int currentYear) {
         this.currentYear = currentYear;
         this.months = new TreeSet<>();
     }
-
+    /**
+     * Retrieves the current year.
+     * 
+     * @return The current year.
+     */
     public int getCurrentYear() {
         return currentYear;
     }
-
+    /**
+     * Sets the current year.
+     * 
+     * @param currentYear The current year to set.
+     */
     public void setCurrentYear(int currentYear) {
         this.currentYear = currentYear;
     }
-
+    /**
+     * Retrieves the set of months in the year.
+     * 
+     * @return The set of months.
+     */
     public TreeSet<Month> getMonths() {
         return months;
     }
-    
-    // adding month to the year
+    /**
+     * Adds a month to the set of months in the year.
+     * 
+     * @param month The month to add.
+     */
     public void addMonth(Month month) {
     	this.months.add(month);
     }
-
+    /**
+     * Sets the set of months in the year.
+     * 
+     * @param months The set of months to set.
+     */
     public void setMonths(TreeSet<Month> months) {
         this.months = months;
     }
-
+    /**
+     * Compares this Year object with another Year object based on their current years.
+     * 
+     * @param other The Year object to compare with.
+     * @return A negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
+     */
     @Override
     public int compareTo(Year other) {
         return Integer.compare(this.currentYear, other.getCurrentYear());
     }
-    
-    // finding the month by number
+    /**
+     * Finds and returns the month with the specified month number.
+     * 
+     * @param monthNumber The number of the month to find.
+     * @return The Month object with the specified month number.
+     * @throws MonthNotFoundException If the specified month number is not found in the year.
+     */
     public Month findMonthByNumber(int monthNumber) throws MonthNotFoundException {
     	for (Month month : months) {
     		if (month.getMonthNumber() == monthNumber) {
